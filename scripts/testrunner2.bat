a26a4dd9c568c83308339e2b9c4891bd23d169d0
KUBO Atsuhiro <kubo@iteman.jp>
2007-07-12 02:25:21 +0000

    - Rewrote code based on symfony.bat

--- scripts/testrunner2.bat
+++ scripts/testrunner2.bat
@@ -1,16 +1,43 @@
 @ECHO OFF
 REM $Id: pearenv.bat 15 2007-05-26 17:31:39Z iteman $
 
-REM %~dp0 is expanded pathname of the current script under NT
-set SCRIPT_DIR=%~dp0
+REM *************************************************************
+REM ** Stagehand_TestRunner CLI for Windows based systems (based on symfony.bat)
+REM *************************************************************
 
-IF "%PHP_PEAR_PHP_BIN%"=="" SET "PHP_PEAR_PHP_BIN=@php_bin@"
+REM This script will do the following:
+REM - check for PHP_COMMAND env, if found, use it.
+REM   - if not found detect php, if found use it, otherwise err and terminate
+
+IF "%OS%"=="Windows_NT" @SETLOCAL
+
+REM %~dp0 is expanded pathname of the current script under NT
+SET SCRIPT_DIR=%~dp0
+
+GOTO INIT
+
+:INIT
+
+IF "%PHP_COMMAND%" == "" GOTO NO_PHPCOMMAND
 
 IF EXIST ".\testrunner2" (
-  %PHP_PEAR_PHP_BIN% -d html_errors=off -d open_basedir= -q ".\testrunner2" %1 %2 %3 %4 %5 %6 %7 %8 %9
+  %PHP_COMMAND% -d html_errors=off -d open_basedir= -q ".\testrunner2" %1 %2 %3 %4 %5 %6 %7 %8 %9
 ) ELSE (
-  %PHP_PEAR_PHP_BIN% -d html_errors=off -d open_basedir= -q "%SCRIPT_DIR%testrunner2" %1 %2 %3 %4 %5 %6 %7 %8 %9
+  %PHP_COMMAND% -d html_errors=off -d open_basedir= -q "%SCRIPT_DIR%\testrunner2" %1 %2 %3 %4 %5 %6 %7 %8 %9
 )
+GOTO CLEANUP
+
+:NO_PHPCOMMAND
+REM ECHO ------------------------------------------------------------------------
+REM ECHO WARNING: Set environment var PHP_COMMAND to the location of your php.exe
+REM ECHO          executable (e.g. C:\PHP\php.exe).  (assuming php.exe on PATH)
+REM ECHO ------------------------------------------------------------------------
+SET PHP_COMMAND=php.exe
+GOTO INIT
+
+:CLEANUP
+IF "%OS%"=="Windows_NT" @ENDLOCAL
+REM PAUSE
 
 REM Local Variables:
 REM mode: bat-generic
