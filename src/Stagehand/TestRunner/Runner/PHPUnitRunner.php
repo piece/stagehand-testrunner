fc8459b57b3f9fbd4452c2b2ad66531088b4c199
KUBO Atsuhiro <kubo@iteman.jp>
2010-08-29 00:52:18 +0900

    Improved some regular expressions.

--- src/Stagehand/TestRunner/Runner/PHPUnitRunner.php
+++ src/Stagehand/TestRunner/Runner/PHPUnitRunner.php
@@ -119,10 +119,10 @@ class Stagehand_TestRunner_Runner_PHPUnitRunner extends Stagehand_TestRunner_Run
             if (preg_match('/^(?:\x1b\[30;42m\x1b\[2K)?(OK .+)/m', $output, $matches)) {
                 $this->notification->name = 'Green';
                 $this->notification->description = $matches[1];
-            } elseif (preg_match('/^(?:\x1b\[37;41m\x1b\[2K)?(FAILURES!)\s(?:\x1b\[0m\x1b\[37;41m\x1b\[2K)?(.+)/m', $output, $matches)) {
+            } elseif (preg_match('/^(?:\x1b\[37;41m\x1b\[2K)?(FAILURES!)\s^(?:\x1b\[0m\x1b\[37;41m\x1b\[2K)?(.+)/m', $output, $matches)) {
                 $this->notification->name = 'Red';
                 $this->notification->description = $matches[1] . "\n" . $matches[2];
-            } elseif (preg_match('/^(?:\x1b\[30;43m\x1b\[2K)?(OK, but incomplete or skipped tests!)\s(?:\x1b\[0m\x1b\[30;43m\x1b\[2K)?(.+)/m', $output, $matches)) {
+            } elseif (preg_match('/^(?:\x1b\[30;43m\x1b\[2K)?(OK, but incomplete or skipped tests!)\s^(?:\x1b\[0m\x1b\[30;43m\x1b\[2K)?(.+)/m', $output, $matches)) {
                 $this->notification->name = 'Red';
                 $this->notification->description = $matches[1] . "\n" . $matches[2];
             }
