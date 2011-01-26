5e7c9550b6c9f9793306af9bf7f98bf47a908f0d
KUBO Atsuhiro <kubo@iteman.jp>
2011-01-27 02:05:07 +0900

    Refactored.

--- src/Stagehand/TestRunner/Preparator/PHPUnitPreparator.php
+++ src/Stagehand/TestRunner/Preparator/PHPUnitPreparator.php
@@ -48,28 +48,9 @@ class Stagehand_TestRunner_Preparator_PHPUnitPreparator extends Stagehand_TestRu
 {
     public function prepare()
     {
-        if (version_compare(PHPUnit_Runner_Version::id(), '3.5.0beta1', '>=')) {
-            require_once 'PHPUnit/Autoload.php';
-        } else {
-            require_once 'PHPUnit/Framework.php';
-        }
-
+        $this->prepareFramework();
         if (!is_null($this->config->phpunitConfigFile)) {
-            require_once 'PHPUnit/Util/Configuration.php';
-            $phpunitConfiguration = PHPUnit_Util_Configuration::getInstance($this->config->phpunitConfigFile)->getPHPUnitConfiguration();
-            if (array_key_exists('bootstrap', $phpunitConfiguration)) {
-                if (array_key_exists('syntaxCheck', $phpunitConfiguration)) {
-                    $this->handleBootstrap($phpunitConfiguration['bootstrap'], $phpunitConfiguration['syntaxCheck']);
-                } else {
-                    $this->handleBootstrap($phpunitConfiguration['bootstrap']);
-                }
-            }
-
-            $browsers = PHPUnit_Util_Configuration::getInstance($this->config->phpunitConfigFile)->getSeleniumBrowserConfiguration();
-            if (count($browsers)) {
-                require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
-                PHPUnit_Extensions_SeleniumTestCase::$browsers = $browsers;
-            }
+            $this->earlyConfigure();
         }
     }
 
@@ -87,6 +68,40 @@ class Stagehand_TestRunner_Preparator_PHPUnitPreparator extends Stagehand_TestRu
             PHPUnit_Util_Fileloader::checkAndLoad($filename, $syntaxCheck);
         } catch (RuntimeException $e) {
             PHPUnit_TextUI_TestRunner::showError($e->getMessage());
+        }
+    }
+
+    /**
+     * @since Method available since Release 2.16.0
+     */
+    protected function prepareFramework()
+    {
+        if (version_compare(PHPUnit_Runner_Version::id(), '3.5.0beta1', '>=')) {
+            require_once 'PHPUnit/Autoload.php';
+        } else {
+            require_once 'PHPUnit/Framework.php';
+        }
+    }
+
+    /**
+     * @since Method available since Release 2.16.0
+     */
+    protected function earlyConfigure()
+    {
+        require_once 'PHPUnit/Util/Configuration.php';
+        $phpunitConfiguration = PHPUnit_Util_Configuration::getInstance($this->config->phpunitConfigFile)->getPHPUnitConfiguration();
+        if (array_key_exists('bootstrap', $phpunitConfiguration)) {
+            if (array_key_exists('syntaxCheck', $phpunitConfiguration)) {
+                $this->handleBootstrap($phpunitConfiguration['bootstrap'], $phpunitConfiguration['syntaxCheck']);
+            } else {
+                $this->handleBootstrap($phpunitConfiguration['bootstrap']);
+            }
+        }
+
+        $browsers = PHPUnit_Util_Configuration::getInstance($this->config->phpunitConfigFile)->getSeleniumBrowserConfiguration();
+        if (count($browsers)) {
+            require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
+            PHPUnit_Extensions_SeleniumTestCase::$browsers = $browsers;
         }
     }
 }
