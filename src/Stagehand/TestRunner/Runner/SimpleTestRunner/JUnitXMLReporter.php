475b6e8b1335cfd6f2ae74efd5bb0370f29649f3
KUBO Atsuhiro <kubo@iteman.jp>
2011-02-06 20:25:44 +0900

    Removed useless stack trace for failures.

--- src/Stagehand/TestRunner/Runner/SimpleTestRunner/JUnitXMLReporter.php
+++ src/Stagehand/TestRunner/Runner/SimpleTestRunner/JUnitXMLReporter.php
@@ -4,7 +4,7 @@
 /**
  * PHP version 5
  *
- * Copyright (c) 2009-2010 KUBO Atsuhiro <kubo@iteman.jp>,
+ * Copyright (c) 2009-2011 KUBO Atsuhiro <kubo@iteman.jp>,
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,7 +29,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  * @package    Stagehand_TestRunner
- * @copyright  2009-2010 KUBO Atsuhiro <kubo@iteman.jp>
+ * @copyright  2009-2011 KUBO Atsuhiro <kubo@iteman.jp>
  * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
  * @version    Release: @package_version@
  * @link       http://simpletest.org/
@@ -38,7 +38,7 @@
 
 /**
  * @package    Stagehand_TestRunner
- * @copyright  2009-2010 KUBO Atsuhiro <kubo@iteman.jp>
+ * @copyright  2009-2011 KUBO Atsuhiro <kubo@iteman.jp>
  * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
  * @version    Release: @package_version@
  * @link       http://simpletest.org/
@@ -247,10 +247,7 @@ class Stagehand_TestRunner_Runner_SimpleTestRunner_JUnitXMLReporter extends Simp
      */
     protected function paintFailureOrError($message, $failureOrError)
     {
-        $this->xmlWriter->{ 'write' . $failureOrError }(
-            $message . "\n\n" .
-            $this->buildFailureTrace(debug_backtrace())
-        );
+        $this->xmlWriter->{ 'write' . $failureOrError }($message);
     }
 }
 
