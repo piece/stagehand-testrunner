5b0feccc49996d25829292e7a1fefc229c9cca09
KUBO Atsuhiro <kubo@iteman.jp>
2011-02-06 20:05:33 +0900

    Adjusted to the new title.

--- examples/Stagehand/TestRunner/SimpleTestWebPageTest.php
+++ examples/Stagehand/TestRunner/SimpleTestWebPageTest.php
@@ -4,7 +4,7 @@
 /**
  * PHP version 5
  *
- * Copyright (c) 2010 KUBO Atsuhiro <kubo@iteman.jp>,
+ * Copyright (c) 2010-2011 KUBO Atsuhiro <kubo@iteman.jp>,
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,7 +29,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  * @package    Stagehand_TestRunner
- * @copyright  2010 KUBO Atsuhiro <kubo@iteman.jp>
+ * @copyright  2010-2011 KUBO Atsuhiro <kubo@iteman.jp>
  * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
  * @version    Release: @package_version@
  * @since      File available since Release 2.14.0
@@ -39,7 +39,7 @@ if (!@include_once 'simpletest/web_tester.php') return;
 
 /**
  * @package    Stagehand_TestRunner
- * @copyright  2010 KUBO Atsuhiro <kubo@iteman.jp>
+ * @copyright  2010-2011 KUBO Atsuhiro <kubo@iteman.jp>
  * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
  * @version    Release: @package_version@
  * @since      Class available since Release 2.14.0
@@ -51,7 +51,7 @@ class Stagehand_TestRunner_SimpleTestWebPageTest extends WebTestCase
     }
 
     public function testIsPass() {
-        $this->assertTitle('Example Web Page');
+        $this->assertTitle('IANA &mdash; Example domains');
     }
 }
 
