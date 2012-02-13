eca0813ff9e8cb5e3413467eea6ec41978b85f48
KUBO Atsuhiro <kubo@iteman.jp>
2012-02-14 03:24:03 +0900

    Added tests for JUnit XML writing for PHPSpec. (Issue #340)

--- test/Stagehand/TestRunner/Runner/JUnitXMLWriting/StreamingPHPSpecRunner.php
+++ test/Stagehand/TestRunner/Runner/JUnitXMLWriting/StreamingPHPSpecRunner.php
@@ -4,7 +4,7 @@
 /**
  * PHP version 5.3
  *
- * Copyright (c) 2008-2012 KUBO Atsuhiro <kubo@iteman.jp>,
+ * Copyright (c) 2012 KUBO Atsuhiro <kubo@iteman.jp>,
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,38 +29,39 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  * @package    Stagehand_TestRunner
- * @copyright  2008-2012 KUBO Atsuhiro <kubo@iteman.jp>
+ * @copyright  2012 KUBO Atsuhiro <kubo@iteman.jp>
  * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
  * @version    Release: @package_version@
- * @since      File available since Release 2.2.0
+ * @since      File available since Release 3.0.0
  */
 
-namespace Stagehand\TestRunner;
+namespace Stagehand\TestRunner\Runner\JUnitXMLWriting;
 
-use PHPSpec\Context;
+use Stagehand\TestRunner\Runner\PHPSpecRunner;
 
 /**
- * TestCase for the PHPSpec runner.
- *
  * @package    Stagehand_TestRunner
- * @copyright  2008-2012 KUBO Atsuhiro <kubo@iteman.jp>
+ * @copyright  2012 KUBO Atsuhiro <kubo@iteman.jp>
  * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
  * @version    Release: @package_version@
- * @since      Class available since Release 2.2.0
+ * @since      Class available since Release 3.0.0
  */
-abstract class PHPSpecCommon extends Context
+class StreamingPHPSpecRunner extends PHPSpecRunner implements JUnitXMLStreamTester
 {
-    public function itShouldPassCommon()
+    /**
+     * @var \Stagehand\TestRunner\Runner\JUnitXMLStreamRecorder
+     */
+    protected $junitXMLStreamRecorder;
+
+    public function getJUnitXMLStreamRecorder()
     {
-        $this->spec(true)->should->beTrue();
+        return $this->junitXMLStreamRecorder;
     }
 
-    /**
-     * @since Method available since Release 3.0.0
-     */
-    public function itShouldFailCommon()
+    protected function createStreamWriter($file)
     {
-        $this->spec(false)->should->beTrue();
+        $this->junitXMLStreamRecorder = new JUnitXMLStreamRecorder($file);
+        return $this->junitXMLStreamRecorder;
     }
 }
 
