1fca778101fda27ed24f09304de545b30bf722c6
KUBO Atsuhiro <kubo@iteman.jp>
2012-02-20 14:26:59 +0900

    Introduced Stagehand_AlterationMonitor 2.0.0 and set 100 milliseconds as the scan interval.

--- src/Stagehand/TestRunner/Process/AlterationMonitoring.php
+++ src/Stagehand/TestRunner/Process/AlterationMonitoring.php
@@ -4,7 +4,7 @@
 /**
  * PHP version 5.3
  *
- * Copyright (c) 2011 KUBO Atsuhiro <kubo@iteman.jp>,
+ * Copyright (c) 2011-2012 KUBO Atsuhiro <kubo@iteman.jp>,
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,7 +29,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  * @package    Stagehand_TestRunner
- * @copyright  2011 KUBO Atsuhiro <kubo@iteman.jp>
+ * @copyright  2011-2012 KUBO Atsuhiro <kubo@iteman.jp>
  * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
  * @version    Release: @package_version@
  * @since      File available since Release 3.0.0
@@ -37,9 +37,11 @@
 
 namespace Stagehand\TestRunner\Process;
 
+use Stagehand\AlterationMonitor\AlterationMonitor;
+
 /**
  * @package    Stagehand_TestRunner
- * @copyright  2011 KUBO Atsuhiro <kubo@iteman.jp>
+ * @copyright  2011-2012 KUBO Atsuhiro <kubo@iteman.jp>
  * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
  * @version    Release: @package_version@
  * @since      Class available since Release 3.0.0
@@ -52,7 +54,7 @@ class AlterationMonitoring
      */
     public function monitor(array $monitoringDirectories, $callback)
     {
-        $monitor = new \Stagehand_AlterationMonitor($monitoringDirectories, $callback);
+        $monitor = new AlterationMonitor($monitoringDirectories, 100000, $callback);
         $monitor->monitor();
     }
 }
