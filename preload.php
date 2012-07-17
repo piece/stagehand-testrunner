331742ec783f5a3f6c1d7f38d4f01e09ae7ab72e
KUBO Atsuhiro <kubo@iteman.jp>
2012-07-17 11:00:03 +0900

    [Composer] Changed the path of the autoload file.

--- preload.php
+++ preload.php
@@ -37,7 +37,7 @@
 
 error_reporting(error_reporting() & ~E_STRICT & ~E_DEPRECATED);
 
-require_once __DIR__ . '/vendor/.composer/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 /*
  * Local Variables:
