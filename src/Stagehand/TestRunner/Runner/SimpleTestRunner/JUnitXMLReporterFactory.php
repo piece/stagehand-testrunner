febb31c4b70f9a0b70cf26204805c5f1940fd2fa
KUBO Atsuhiro <kubo@iteman.jp>
2013-03-12 21:53:10 +0900

    [DependencyInjection] Changed the super type of the JUnitXMLReporterFactory class to the ComponentAwareFactory.

--- src/Stagehand/TestRunner/Runner/SimpleTestRunner/JUnitXMLReporterFactory.php
+++ src/Stagehand/TestRunner/Runner/SimpleTestRunner/JUnitXMLReporterFactory.php
@@ -4,7 +4,7 @@
 /**
  * PHP version 5.3
  *
- * Copyright (c) 2011-2012 KUBO Atsuhiro <kubo@iteman.jp>,
+ * Copyright (c) 2011-2013 KUBO Atsuhiro <kubo@iteman.jp>,
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,7 +29,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  * @package    Stagehand_TestRunner
- * @copyright  2011-2012 KUBO Atsuhiro <kubo@iteman.jp>
+ * @copyright  2011-2013 KUBO Atsuhiro <kubo@iteman.jp>
  * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
  * @version    Release: @package_version@
  * @link       http://simpletest.org/
@@ -38,19 +38,19 @@
 
 namespace Stagehand\TestRunner\Runner\SimpleTestRunner;
 
-use Stagehand\TestRunner\Core\Plugin\PluginAwareFactory;
+use Stagehand\ComponentFactory\ComponentAwareFactory;
 use Stagehand\TestRunner\JUnitXMLWriter\JUnitXMLWriter;
 use Stagehand\TestRunner\TestSuite\SimpleTestTestSuite;
 
 /**
  * @package    Stagehand_TestRunner
- * @copyright  2011-2012 KUBO Atsuhiro <kubo@iteman.jp>
+ * @copyright  2011-2013 KUBO Atsuhiro <kubo@iteman.jp>
  * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
  * @version    Release: @package_version@
  * @link       http://simpletest.org/
  * @since      Class available since Release 3.0.0
  */
-class JUnitXMLReporterFactory extends PluginAwareFactory
+class JUnitXMLReporterFactory extends ComponentAwareFactory
 {
     /**
      * @param \Stagehand\TestRunner\JUnitXMLWriter\JUnitXMLWriter $junitXMLWriter
