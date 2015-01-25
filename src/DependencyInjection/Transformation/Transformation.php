8e1cf53bd9a3dd6a0fe88f180a7345e293a2dc7f
KUBO Atsuhiro <kubo@iteman.jp>
2015-01-25 11:59:24 +0900

    change Transformation to avoid an ErrorException when running a test with Symfony 2.7 #42
    [ErrorException]
    The ability to pass file names to the Symfony\Component\Yaml\Yaml::parse method is deprecated since version 2.2 and will be removed in 3.0. Pass the YAML contents of the file instead.

--- src/DependencyInjection/Transformation/Transformation.php
+++ src/DependencyInjection/Transformation/Transformation.php
@@ -4,7 +4,7 @@
 /**
  * PHP version 5.3
  *
- * Copyright (c) 2011-2013 KUBO Atsuhiro <kubo@iteman.jp>,
+ * Copyright (c) 2011-2013, 2015 KUBO Atsuhiro <kubo@iteman.jp>,
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -29,7 +29,7 @@
  * POSSIBILITY OF SUCH DAMAGE.
  *
  * @package    Stagehand_TestRunner
- * @copyright  2011-2013 KUBO Atsuhiro <kubo@iteman.jp>
+ * @copyright  2011-2013, 2015 KUBO Atsuhiro <kubo@iteman.jp>
  * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
  * @version    Release: @package_version@
  * @since      File available since Release 3.0.0
@@ -47,7 +47,7 @@ use Stagehand\TestRunner\Util\String;
 
 /**
  * @package    Stagehand_TestRunner
- * @copyright  2011-2013 KUBO Atsuhiro <kubo@iteman.jp>
+ * @copyright  2011-2013, 2015 KUBO Atsuhiro <kubo@iteman.jp>
  * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
  * @version    Release: @package_version@
  * @since      Class available since Release 3.0.0
@@ -112,7 +112,7 @@ class Transformation
     public function transformToContainerParameters()
     {
         if (!is_null($this->configurationFile)) {
-            foreach (Yaml::parse($this->configurationFile) as $configurationID => $configurationPart) {
+            foreach (Yaml::parse(file_get_contents($this->configurationFile)) as $configurationID => $configurationPart) {
                 if (!is_null($configurationPart)) {
                     if (!array_key_exists($configurationID, $this->configuration)) {
                         $this->configuration[$configurationID] = array();
